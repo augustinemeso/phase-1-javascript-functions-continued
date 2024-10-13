aa0769293de29f814f9034d56883b6b8280c34c0
augustinemeso <augustinemesobk@gmail.com>
2024-10-13 23:25:38 +0300

    add phase-1-javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+// Function to implement Saturday fun activity
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function to implement Monday work activity
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function that returns a function with a visual flair
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Example usage of the functions
+  console.log(saturdayFun()); //=> "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("hike")); //=> "This Saturday, I want to hike!"
+  
+  console.log(mondayWork()); //=> "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); //=> "This Monday, I will work from home."
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); //=> "You are !!!a dedicated programmer!!!"
+  console.log(wrapAdjective("%")("a dedicated programmer")); //=> "You are %a dedicated programmer%!"
+  
